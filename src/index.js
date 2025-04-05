8dfa6b8fa3ce00851f16f4504a52edb5ec0cb396
alecia00 <88971654+alecia00@users.noreply.github.com>
2025-04-05 16:13:28 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globals.css';
+import './styles/components.css';
+import './styles/wallet-modal.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
